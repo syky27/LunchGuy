2652bcc3c50533ec3d9cf759e2213f321d9f7f31
Tomas Sykora, jr <jump@syky.cz>
2016-05-24 00:27:38 +0200

    LIB: Alamofire

---
+++ LunchGuy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LunchGuy
+//
+//  Created by Tomas Sykora, jr. on 24/05/16.
+//  Copyright © 2016 AJTY, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
