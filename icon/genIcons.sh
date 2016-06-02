d8cb9f253d65131efcb63e229b04ddb68bb49d93
Miro Hrončok <miro@hroncok.cz>
2016-06-02 11:50:44 +0200

    FIX: Modify both the SVG and the script to actually work

--- icon/genIcons.sh
+++ icon/genIcons.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 icon2png() {
-  convert -density 1200 -resize "$1" icon.svg "$2"
+  echo -n "$1: "
+  convert -density "$1" -print "%wx%h\n" icon.svg "$2"
 }
 
-icon2png 40x40 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40.png"
-icon2png 80x80 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40@2x.png"
+icon2png 40 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40.png"
+icon2png 80 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40@2x.png"
 
-icon2png 120x120 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40@3x.png"
+icon2png 120 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40@3x.png"
 
-icon2png 120x120 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-60@2x.png"
-icon2png 180x180 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-60@3x.png"
+icon2png 120 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-60@2x.png"
+icon2png 180 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-60@3x.png"
 
-icon2png 72x72 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-72.png"
-icon2png 144x144 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-72@2x.png"
+icon2png 72 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-72.png"
+icon2png 144 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-72@2x.png"
 
-icon2png 76x76 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-76.png"
-icon2png 152x152 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-76@2x.png"
-icon2png 167x167 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-83.5@3x.png"
+icon2png 76 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-76.png"
+icon2png 152 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-76@2x.png"
+icon2png 167 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-83.5@2x.png"
 
-icon2png 50x50 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small-50.png"
-icon2png 100x100 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small-50@2x.png"
+icon2png 50 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small-50.png"
+icon2png 100 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small-50@2x.png"
 
-icon2png 29x29 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small.png"
-icon2png 58x58 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small@2x.png"
-icon2png 87x87 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small@3x.png"
+icon2png 29 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small.png"
+icon2png 58 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small@2x.png"
+icon2png 87 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small@3x.png"
 
-icon2png 27x27 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon.png"
-icon2png 54x54 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon@2x.png"
+icon2png 57 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon.png"
+icon2png 114 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon@2x.png"
