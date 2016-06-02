5f9fcb6af9e14c8fa06798d9d62a191313100dc7
Tomas Sykora, jr <jump@syky.cz>
2016-06-02 02:07:31 +0200

    FEAT: Use imagemagic

--- icon/genIcons.sh
+++ icon/genIcons.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-qlmanage -t -s 2048 -o . icon.svg && 
+icon2png() {
+  convert -density 1200 -resize "$1" icon.svg "$2"
+}
 
-sips --resampleWidth 40 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40.png"
-sips --resampleWidth 80 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40@2x.png"
-sips --resampleWidth 120 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40@3x.png"
+icon2png 40x40 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40.png"
+icon2png 80x80 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40@2x.png"
 
-sips --resampleWidth 120 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-60@2x.png"
-sips --resampleWidth 180 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-60@3x.png"
+icon2png 120x120 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-40@3x.png"
 
-sips --resampleWidth 72 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-72.png"
-sips --resampleWidth 144 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-72@2x.png"
+icon2png 120x120 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-60@2x.png"
+icon2png 180x180 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-60@3x.png"
 
-sips --resampleWidth 76 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-76.png"
-sips --resampleWidth 152 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-76@2x.png"
-sips --resampleWidth 167 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-83.5@3x.png"
+icon2png 72x72 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-72.png"
+icon2png 144x144 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-72@2x.png"
 
-sips --resampleWidth 50 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small-50.png"
-sips --resampleWidth 100 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small-50@2x.png"
+icon2png 76x76 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-76.png"
+icon2png 152x152 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-76@2x.png"
+icon2png 167x167 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-83.5@3x.png"
 
+icon2png 50x50 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small-50.png"
+icon2png 100x100 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small-50@2x.png"
 
-sips --resampleWidth 29 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small.png"
-sips --resampleWidth 58 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small@2x.png"
-sips --resampleWidth 87 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small@3x.png"
+icon2png 29x29 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small.png"
+icon2png 58x58 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small@2x.png"
+icon2png 87x87 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon-Small@3x.png"
 
-sips --resampleWidth 27 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon.png"
-sips --resampleWidth 54 icon.svg.png --out "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon@2x.png"
+icon2png 27x27 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon.png"
+icon2png 54x54 "../LunchGuy/Assets.xcassets/AppIcon.appiconset/Icon@2x.png"
