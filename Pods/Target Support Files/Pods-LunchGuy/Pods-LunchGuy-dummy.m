0cffdf9f11d624053f98e944dad20265d4f5847b
Josef Dolezal <pepik.ml@gmail.com>
2017-10-23 18:10:27 +0200

    REF: Remove Pods directory from repository

--- Pods/Target Support Files/Pods-LunchGuy/Pods-LunchGuy-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LunchGuy : NSObject
-@end
-@implementation PodsDummy_Pods_LunchGuy
-@end
--- Pods/Target Support Files/Pods-LunchGuyUITests/Pods-LunchGuyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LunchGuyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_LunchGuyUITests
-@end
