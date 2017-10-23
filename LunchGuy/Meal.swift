3b032f0663fa91ba4b5d8a47080886a20909e7ed
Josef Dolezal <pepik.ml@gmail.com>
2017-10-23 22:38:54 +0200

    REF: Refactor model objects into separate files, add support for meal categories

---
+++ LunchGuy/Meal.swift
@@ -0,0 +1,14 @@
+//
+//  Meal.swift
+//  LunchGuy
+//
+//  Created by Josef Dolezal on 23/10/2017.
+//  Copyright © 2017 AJTY, s.r.o. All rights reserved.
+//
+
+import Foundation
+
+struct Meal {
+    let name: String
+    let price: Int?
+}
---
+++ LunchGuy/Menu.swift
@@ -0,0 +1,14 @@
+//
+//  Menu.swift
+//  LunchGuy
+//
+//  Created by Josef Dolezal on 23/10/2017.
+//  Copyright © 2017 AJTY, s.r.o. All rights reserved.
+//
+
+import Foundation
+
+struct Menu {
+    let category: String
+    let meals: [Meal]
+}
