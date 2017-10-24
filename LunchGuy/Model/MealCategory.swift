8b2b96d8ca6d6b712250aa2c3cac58f50233216b
Josef Dolezal <pepik.ml@gmail.com>
2017-10-24 19:16:04 +0200

    FEAT: Create MealCategory model

---
+++ LunchGuy/Model/MealCategory.swift
@@ -0,0 +1,14 @@
+//
+//  MealCategory.swift
+//  LunchGuy
+//
+//  Created by Josef Dolezal on 24/10/2017.
+//  Copyright © 2017 AJTY, s.r.o. All rights reserved.
+//
+
+import Foundation
+
+struct MealCategory {
+    let category: String
+    let meals: [Meal]
+}
