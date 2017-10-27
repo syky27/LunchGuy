e07898a30e5666b09a3ff1bb090ac1fbfacc77e1
Josef Dolezal <pepik.ml@gmail.com>
2017-10-27 20:01:20 +0200

    FEAT: Create restaurants map controller

---
+++ LunchGuy/ViewControllers/RestaurantsMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantsMapViewController.swift
+//  LunchGuy
+//
+//  Created by Josef Dolezal on 27/10/2017.
+//  Copyright © 2017 AJTY, s.r.o. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantsMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
