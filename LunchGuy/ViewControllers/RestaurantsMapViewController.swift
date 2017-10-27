e676bb115041d63183d3567c77c1cbf68f72587d
Josef Dolezal <pepik.ml@gmail.com>
2017-10-28 01:16:58 +0200

    UI: Add map view pin callout action

--- LunchGuy/ViewControllers/RestaurantsMapViewController.swift
+++ LunchGuy/ViewControllers/RestaurantsMapViewController.swift
@@ -9,17 +9,13 @@
 import UIKit
 import MapKit
 
-class RestaurantsMapViewController: UIViewController {
-
-    struct LocatedRestaurant {
-        let restaurant: Restaurant
-        let coordinates: CLLocationCoordinate2D
-    }
+class RestaurantsMapViewController: UIViewController, MKMapViewDelegate {
 
     private let mapView = MKMapView()
     private let locationProvider = RestaurantStaticLocationProvider()
     private let restaurantsDataSource: RestaurantsDataSource
-    private var locatedRestaurants = [LocatedRestaurant]()
+
+    private var selectedRestaurant: Restaurant?
 
     init(restaurantsDataSource: RestaurantsDataSource) {
         self.restaurantsDataSource = restaurantsDataSource
@@ -50,30 +46,75 @@ class RestaurantsMapViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        mapView.delegate = self
+
         locateRestaurants()
     }
 
-    private func locateRestaurants() {
-        locatedRestaurants = restaurantsDataSource.restaurants.flatMap { restaurant in
-            guard let coordinates = locationProvider.coordinates(for: restaurant) else { return nil }
+    // MARK: MKMapViewDelegate
 
-            return LocatedRestaurant(restaurant: restaurant, coordinates: coordinates)
-        }
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        guard let annotation = view.annotation as? RestaurantAnnotation else { return }
 
-        renderAnnotations()
+        selectedRestaurant = annotation.restaurant
     }
 
-    private func renderAnnotations() {
+    func mapView(_ mapView: MKMapView, didDeselect view: MKAnnotationView) {
+        selectedRestaurant = nil
+    }
+
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard
+            !(annotation is MKUserLocation),
+            #available(iOS 11.0, *)
+        else {
+            // Fallback to system default
+            return nil
+        }
+
+        let markerView
+            = (mapView.dequeueReusableAnnotationView(withIdentifier: "markerView") as? MKMarkerAnnotationView)
+            ?? MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "markerView")
+        let detailButton = UIButton(type: .detailDisclosure)
+
+        detailButton.addTarget(self, action: #selector(openRestaurantMenuHandler), for: .touchUpInside)
+
+        markerView.canShowCallout = true
+        markerView.rightCalloutAccessoryView = detailButton
+
+        return markerView
+    }
+
+    // MARK: UI callbacks
+
+    @objc func openRestaurantMenuHandler() {
+        guard let restaurant = selectedRestaurant else { return }
+
+        let controller = MenuTableViewController()
+        controller.restaurant = restaurant
+
+        navigationController?.pushViewController(controller, animated: true)
+    }
+
+    // MARK: Internal logic
+
+    private func locateRestaurants() {
+        // Get coordinates of restaurant and create annotation if it's possible
+        let annotations = restaurantsDataSource.restaurants.flatMap { restaurant -> RestaurantAnnotation? in
+            guard let coordinate = locationProvider.coordinates(for: restaurant) else { return nil }
+
+            return RestaurantAnnotation(restaurant: restaurant, coordinate: coordinate)
+        }
+
+        render(annotations: annotations)
+    }
+
+    private func render(annotations: [MKAnnotation]) {
         // Remove all map annotations currently rendered
         mapView.removeAnnotations(mapView.annotations)
 
         // Render all annotations on clean canvas
-        locatedRestaurants.forEach { locatedRestaurant in
-            let annotation = MKPointAnnotation()
-
-            annotation.coordinate = locatedRestaurant.coordinates
-            mapView.addAnnotation(annotation)
-        }
+        mapView.addAnnotations(annotations)
     }
 }
 
